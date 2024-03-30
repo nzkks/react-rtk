57d4afdb3f718d8580da45ba93d8d1c978d0e2df
nzkks <kkshanthosh@gmail.com>
2024-03-30 16:20:04 +1300

    cakes and Ice creams views: Show the numbers available

--- src/features/icecream/IceCreamView.jsx
+++ src/features/icecream/IceCreamView.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export const IceCreamView = () => {
+  const numOfIceCreams = useSelector(state => state.iceCream.numOfIceCreams);
+
   return (
     <div>
-      <h2>Number of iceCreams - </h2>
+      <h2>Number of iceCreams - {numOfIceCreams}</h2>
       <button>Order ice cream</button>
       <button>Restock iceCreams</button>
     </div>
