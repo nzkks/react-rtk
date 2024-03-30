413aa0640601b0b271d339f3cc5596c7559de53d
nzkks <kkshanthosh@gmail.com>
2024-03-30 16:55:46 +1300

    UseerView: Show the fetched users names

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -9,7 +9,7 @@ const initialState = {
 
 // Generates pending, fulfilled and rejected action types
 export const fetchUsers = createAsyncThunk('user/fetchUsers', () => {
-  return axios.get('https://jsonplaceholder.typicode.com/users').then(response => response.data.map(user => user.id));
+  return axios.get('https://jsonplaceholder.typicode.com/users').then(response => response.data);
 });
 
 const userSlice = createSlice({
